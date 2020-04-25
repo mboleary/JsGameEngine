b2ca1eb585ecb4e16f44592dc8b3afbf034b6e5c
Brady O'Leary <thebradyoleary@gmail.com>
2020-04-25 01:14:25 -0400

    Added basic Pubsub server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,53 @@
- 
+/**
+ * PubSub Component
+ */
+
+const WebSocket = require('ws');
+
+function noop() { }
+
+function heartbeat() {
+    this.isAlive = true;
+}
+
+const wss = new WebSocket.Server({ port: 8001 });
+
+wss.on('connection', function connection(ws, req) {
+    console.log("Connection:", req.socket.remoteAddress);
+    ws.isAlive = true;
+    ws.on('pong', heartbeat);
+    ws.on('message', (msg) => {
+        console.log("Message:", msg);
+        if (!msg) return;
+        let json = null;
+        try {
+            json = JSON.parse(msg);
+        } catch (e) {
+            ws.send("{err: \"Not Valid JSON\"");
+            return;
+        }
+        console.log("Action:", json.action, "Target:", json.target);
+        if (json.action === "update") {
+            if (json.target === "*") {
+                wss.clients.forEach((wsr) => {
+                    if (wsr !== ws) {
+                        wsr.send(JSON.stringify(json));
+                    }
+                });
+            }
+        }
+    });
+    ws.send('TEST');
+});
+
+const interval = setInterval(function ping() {
+    wss.clients.forEach(function each(ws) {
+        if (ws.isAlive === false) return ws.terminate();
+        ws.isAlive = false;
+        ws.ping(noop);
+    });
+}, 30000);
+
+wss.on('close', function close() {
+    clearInterval(interval);
+});
